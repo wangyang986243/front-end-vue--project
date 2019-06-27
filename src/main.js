ac2cb7f48c468160710995e3d2c389e1c88dd831
e <wangyang@qq.com>
2019-06-27 21:36:47 +0800

    登入页面样式完成

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+//引入assets下面的index.css文件
+import './assets/css/index.css'
+Vue.use(ElementUI)
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,80 @@
+<template>
+  <el-row type="flex" class="row-bg" justify="center" align="middle">
+    <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6">
+      <div class="grid-content bg-purple-light"></div>
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+        label-position="top"
+      >
+        <el-form-item label="用户名" prop="name">
+          <el-input v-model="ruleForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="ruleForm.password"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')">登入</el-button>
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ruleForm: {
+        name: "",
+        password: ""
+      },
+      rules: {
+        name: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 4, max: 12, message: "长度在 4 到 12 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 4, max: 12, message: "长度在 4 到 12 个字符", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
+
+<style>
+.row-bg {
+  background: #2d434c;
+  height: 100%;
+}
+.el-form {
+  background-color: #fff;
+  border-radius: 10px;
+  padding: 30px 20px;
+  min-width: 400px;
+}
+</style>
+
+
