d7a3ae9d2cf55ae2783a6434e2a2c13494a3b0f8
e <wangyang@qq.com>
2019-07-06 18:21:59 +0800

    实现组件懒加载

--- src/router.js
+++ src/router.js
@@ -1,13 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Login from './views/Login/Login'
-import Home from './views/Home/Home.vue'
+
+//实现路由的懒加载
+/* import Home from './views/Home/Home.vue'
 import User from './views/User/User.vue'
 import Roles from './views/Roles/Roles.vue'
 import Rights from './views/Rights/Rights.vue'
 import Goods from './views/Goods/Goods.vue'
 import AddGoods from './views/Goods/AddGoods.vue'
-import Categories from './views/Goods/Categories.vue'
+import Categories from './views/Goods/Categories.vue' */
+const Home = () => import('./views/Home/Home.vue')
+const User = () => import('./views/User/User.vue')
+const Roles = () => import('./views/Roles/Roles.vue')
+const Rights = () => import('./views/Rights/Rights.vue')
+const Goods = () => import('./views/Goods/Goods.vue')
+const AddGoods = () => import('./views/Goods/AddGoods.vue')
+const Categories = () => import('./views/Goods/Categories.vue')
 
 Vue.use(Router)
 
